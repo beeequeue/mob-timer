37d5773ab7495180e485eca52f23e6126ec2222c
Adam <adam.lj.haglund@gmail.com>
2018-04-16 17:08:48 +0200

    refactor imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/filter'
+import 'rxjs/add/operator/mapTo'
+import 'rxjs/add/operator/mergeMap'
+import 'rxjs/add/operator/ignoreElements'
 
 import 'normalize.css'
 import './index.scss'
--- src/state/index.ts
+++ src/state/index.ts
@@ -1,22 +1,10 @@
 // tslint:disable:interface-over-type-literal
 import { combineEpics, createEpicMiddleware } from 'redux-observable'
 import { combineReducers, createStore, applyMiddleware, compose } from 'redux'
-import 'rxjs/add/operator/do'
-import 'rxjs/add/operator/filter'
-import 'rxjs/add/operator/mapTo'
-import 'rxjs/add/operator/mergeMap'
-import 'rxjs/add/operator/ignoreElements'
 
 import { timerReducers, IStateTimer } from '@state/reducers/timerReducers'
 import { usersReducers, IStateUsers } from '@state/reducers/usersReducers'
 import { timerEpics } from '@state/epics/timerEpics'
-
-declare module 'redux' {
-  // tslint:disable:interface-name
-  export interface Action {
-    payload?: any | undefined | null
-  }
-}
 
 const composeMiddleware =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
